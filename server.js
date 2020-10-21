a2f39eeb5247b451eefe9ea25c229e89d6657b08
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-21 15:04:11 -0400

    added morgan

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const User = require("./userModel.js");
 
 const app = express();
 
+const exerciseController = require("./controllers/exerciseController");
+const indexController = require("./controllers/indexController");
+const statsController = require("./controllers/statsController");
+const apiController = require("./controllers/apiController.js");
+const workoutController = require("./controllers/workoutController");
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,15 +21,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) =>  {
+  res.send("index.html")
+})
+
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/track-my-workout-now',
+  process.env.MONGODB_URI || "mongodb://localhost/track-my-workout-now",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   }
 );
+
+app.use(exerciseController);
+app.use(indexController);
+app.use(statsController);
+app.use(apiController);
+app.use(workoutController);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
