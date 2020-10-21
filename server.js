50dffb061e68c34efb4b542e1ef459a2e52e80fa
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-21 15:10:51 -0400

    changed it

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000;
-
 const app = express();
-
-const exerciseController = require("./controllers/exerciseController");
-const indexController = require("./controllers/indexController");
-const statsController = require("./controllers/statsController");
-const apiController = require("./controllers/apiController.js");
-const workoutController = require("./controllers/workoutController");
-
-app.use(logger("dev"));
-
+const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) =>  {
-  res.send("index.html")
-})
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/track-my-workout-now",
   {
@@ -33,11 +15,46 @@ mongoose.connect(
   }
 );
 
-app.use(exerciseController);
-app.use(indexController);
-app.use(statsController);
-app.use(apiController);
-app.use(workoutController);
+// const express = require("express");
+// const logger = require("morgan");
+// const mongoose = require("mongoose");
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// const exerciseController = require("./controllers/exerciseController");
+// const indexController = require("./controllers/indexController");
+// const statsController = require("./controllers/statsController");
+// const apiController = require("./controllers/apiController.js");
+// const workoutController = require("./controllers/workoutController");
+
+// app.use(logger("dev"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("public"));
+
+// app.get("/", (req, res) =>  {
+//   res.send("index.html")
+// })
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/track-my-workout-now",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   }
+// );
+
+// app.use(exerciseController);
+// app.use(indexController);
+// app.use(statsController);
+// app.use(apiController);
+// app.use(workoutController);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
